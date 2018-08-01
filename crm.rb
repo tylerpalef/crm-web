4c72ed905bcdc5ffca8ffabcb3d68992098a0e26
David Ma <davidma415@gmail.com>
2018-08-01 10:55:20 -0400

    Finished adding about, and links to all the pages on each page

--- crm.rb
+++ crm.rb
@@ -8,3 +8,16 @@ end
 get '/home' do
   erb :index
 end
+
+get '/contacts' do
+  @contacts = Contact.all
+  erb :contacts
+end
+
+get '/about' do
+  erb :about
+end
+
+after do
+  ActiveRecord::Base.connection.close
+end
